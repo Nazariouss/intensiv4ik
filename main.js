2730ba00e9d41a55da576c4a5b24dc067cfefeb4
Nazariousss <boyko.nazar2013@gmail.com>
2024-01-25 20:24:04 +0200

    homework 1.2

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ function canSmoke(age){
     }
 }
 
-console.log(canSmoke(age))
-
 let can_smoke = function(age){
     if (age >= 18) {
         return "Покури братік"
@@ -16,8 +14,6 @@ let can_smoke = function(age){
         return "Дядя витри молоко з губ"
     }
 }
-
-console.log(can_smoke(age))
 
 let csmok = (age) => {
     if (age >= 18) {
@@ -27,14 +23,9 @@ let csmok = (age) => {
     }
 }
 
-console.log(csmok(age))
-
-//Створити функцію перепустку allowedTOIn(name, age, department)
 function allowedTOIn(name, age, department) {
     if (name == 'Толік' && age >= 18 && department == 'QA') {
         return true
     } 
     return false
 }   
-
-console.log(allowedTOIn("Толік", 21, 'QA'))
