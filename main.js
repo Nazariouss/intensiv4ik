852b439a60d6a1877a650ad5e86be133d1da0034
Nazariousss <boyko.nazar2013@gmail.com>
2024-01-25 13:42:01 +0200

    homework 1.1

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log(typeof 5)
+console.log(typeof a)
+console.log(typeof 'a')
+a = null
+console.log(a)
+console.log(typeof {})
+console.log(typeof 123n)
+console.log(typeof Symbol())
+
+a = 5
+b = '5'
+console.log(a == b)
+console.log(a === b)
